d945119b7ce0712d7485068d01227d04e5469f83
CI <CI@jec.ac.jp>
2023-09-27 10:35:53 +0900

    ステージギミック追加

--- Assets/Main/Script/RandomCreat.cs
+++ Assets/Main/Script/RandomCreat.cs
@@ -11,28 +11,31 @@ public class RandomCreat : MonoBehaviour
     private Transform rangeA;
     [SerializeField]
     private Transform rangeB;
+    private int instant_weapon;
 
     
     private void Start()
     {
         WeaponManager.nowWeapons = 0;
+        instant_weapon = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         // 約1秒置きにランダムに生成されるようにする。
-            if (WeaponManager.maxWeapons > WeaponManager.nowWeapons)
-            {
-                // rangeAとrangeBのx座標の範囲内でランダムな数値を作成
-                float x = Random.Range(rangeA.position.x, rangeB.position.x);
-                // rangeAとrangeBのy座標の範囲内でランダムな数値を作成
-                float y = Random.Range(rangeA.position.y, rangeB.position.y);
+        if (WeaponManager.maxWeapons > WeaponManager.nowWeapons)
+        {
+            // rangeAとrangeBのx座標の範囲内でランダムな数値を作成
+            float x = Random.Range(rangeA.position.x, rangeB.position.x);
+            // rangeAとrangeBのy座標の範囲内でランダムな数値を作成
+            float y = Random.Range(rangeA.position.y, rangeB.position.y);
 
-                Instantiate(createPrefab[WeaponManager.nowWeapons % 3], new Vector3(x, y, 0), createPrefab[WeaponManager.nowWeapons % 3].transform.rotation);
+            Instantiate(createPrefab[instant_weapon % 3], new Vector3(x, y, 0), createPrefab[instant_weapon % 3].transform.rotation);
 
-                WeaponManager.nowWeapons++;
-            }
+            instant_weapon++;
+            WeaponManager.nowWeapons++;
+        }
     }
 
 }
--- Assets/Main/Script/trapsController.cs
+++ Assets/Main/Script/trapsController.cs
@@ -7,19 +7,25 @@ public class trapsController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag.StartsWith("Player")) {
+        if (collision.transform.tag.StartsWith("Player"))
+        {
             collision.gameObject.GetComponent<PlayerController>().TakeDamage(1000, PlayerController.Equiment.PUNCH);
+        }
+        if (collision.gameObject.CompareTag("Sword") || collision.gameObject.CompareTag("Axe") || collision.gameObject.CompareTag("Spear"))
+        {
+            Destroy(collision.gameObject);
+            WeaponManager.nowWeapons--;
         }
     }
 }
